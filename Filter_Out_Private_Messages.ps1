e28eff1e92ac8c48a7829bbd4d9bd833c80585bd
I R Nut Smurt <73799093+irnutsmurt@users.noreply.github.com>
2023-08-19 09:44:08 -0700

    Update Filter_Out_Private_Messages.ps1
    changed the output to be meeting_saved_chat_MM_DD_YYYY

--- Filter_Out_Private_Messages.ps1
+++ Filter_Out_Private_Messages.ps1
@@ -1,5 +1,8 @@
 $filePath = "meeting_saved_chat.txt"
-$outputFile = "meeting_filtered_chat.txt"
+# Get the current date and format it
+$currentDate = Get-Date -Format "MM_dd_yyyy"
+$outputFile = "meeting_saved_chat_$currentDate.txt"
+
 
 # Regex to match timestamps like 08:05:05
 $timestampPattern = "^\d{2}:\d{2}:\d{2}"
