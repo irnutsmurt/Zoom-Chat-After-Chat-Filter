b617d112e7999e039a16668c31085c0e4b1bbc3f
I R Nut Smurt <73799093+irnutsmurt@users.noreply.github.com>
2023-08-19 09:33:16 -0700

    Add files via upload

---
+++ Filter_Out_Private_Messages.ps1
@@ -0,0 +1,25 @@
+$filePath = "meeting_saved_chat.txt"
+$outputFile = "meeting_filtered_chat.txt"
+
+# Regex to match timestamps like 08:05:05
+$timestampPattern = "^\d{2}:\d{2}:\d{2}"
+
+$skip = $false
+
+# Read the content line by line, filter it, and then output to a new file
+Get-Content $filePath | ForEach-Object {
+    if ($_ -match $timestampPattern) {
+        # If it's a line starting with a timestamp
+        if ($_ -notmatch "\(Privately\):") {
+            # And it's not a private message
+            $skip = $false
+            $_ # Write the line to output
+        } else {
+            # It's a private message, so start skipping lines
+            $skip = $true
+        }
+    } elseif (!$skip) {
+        # If we're not in the middle of skipping private messages
+        $_ # Write the line to output
+    }
+} | Out-File $outputFile
